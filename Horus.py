3875179c4f8088c68677a0cb1e228d96c276a89c
fanshengyue <fsy19901101@gmail.com>
2017-12-20 09:39:35 +0800

    communicate
    communicate

--- Horus.py
+++ Horus.py
@@ -1,3 +1,7 @@
 # -*- coding:utf-8 -*- 
 from img_proc. import *
 from obj_detect. import *
+import communicate.communicate as commu
+
+communicate=commu.Communicate();
+communicate.connect_scoket();
